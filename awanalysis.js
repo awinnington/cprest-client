6bf6225b501ec8a08d29120150f16ba9fadbdaf7
Adam Winnington <adam@winnington.com>
2020-01-03 17:17:01 -0500

    Summary Output

--- awanalysis.js
+++ awanalysis.js
@@ -18,7 +18,7 @@ let lastYear = (now - (365 * msday))
 let lastSixmonth = (now - (180 * msday))
 let lastMonth = (now - (30 * msday))
 let lastSeven = (now - (7 * msday))
-let testdate = 1560742571250
+let testdate = 0
 
 
 // if (testdate < lastSeven) console.log("One")
@@ -30,6 +30,8 @@ let importedrules = require("./aw-rules")
 let layers = (Object.keys(importedrules))
 let layer = ""
 let awruleobj = {}
+
+
 
 // Let's flatten the rulebase
 
@@ -55,27 +57,63 @@ for (layer of layers) {
 
 //console.log(awruleobj)
 //cp.writeJson(awruleobj, 'aw-flat')
-let encount = 0
-let discount = 0
+
 
 //console.log(Object.keys(awruleobj))
 for (layer of layers) {
+    let encount = 0
+    let discount = 0
+    let zerocount = 0
+    let hitsLastYear = 0
+    let hitsLastSixmonth = 0
+    let hitsLastMonth = 0
+    let hitsLastSeven = 0
+
+    let createdLastYear = 0
+    let createdLastSixmonth = 0
+    let createdLastMonth = 0
+    let createdLastSeven = 0
+
+    let modifiedLastYear = 0
+    let modifiedLastSixmonth = 0
+    let modifiedLastMonth = 0
+    let modifiedLastSeven = 0
+
     //console.log(awruleobj[layer])
+
     awruleobj[layer].forEach(element => {
         testdate = element["meta-info"]["last-modify-time"]["posix"]
-        if (testdate < lastYear) console.log("last year")
-        else if (testdate < lastSixmonth) console.log("last modify in 6-12 month")
-        else if (testdate < lastMonth) console.log("last modify last 1-6 months")
-        else if (testdate < lastSeven) console.log("last modify last 7 days ")
+        if (testdate < lastYear) {
+            //console.log("last year");
+            modifiedLastYear++
+        } else if (testdate < lastSixmonth) {
+            //console.log("last hit in 6-12 month");
+            modifiedLastSixmonth++
+        } else if (testdate < lastMonth) {
+            //console.log("last hit last 1-6 months");
+            modifiedLastMonth++
+        } else if (testdate < lastSeven) {
+            //console.log("last hit last 7 days ");
+            modifiedLastSeven++
+        }
 
     })
 
     awruleobj[layer].forEach(element => {
         testdate = element["meta-info"]["creation-time"]["posix"]
-        if (testdate < lastYear) console.log("last year")
-        else if (testdate < lastSixmonth) console.log("last created in 6-12 month")
-        else if (testdate < lastMonth) console.log("last created last 1-6 months")
-        else if (testdate < lastSeven) console.log("last created last 7 days ")
+        if (testdate < lastYear) {
+            //console.log("last year");
+            createdLastYear++
+        } else if (testdate < lastSixmonth) {
+            //console.log("last hit in 6-12 month");
+            createdLastSixmonth++
+        } else if (testdate < lastMonth) {
+            //console.log("last hit last 1-6 months");
+            createdLastMonth++
+        } else if (testdate < lastSeven) {
+            //console.log("last hit last 7 days ");
+            createdLastSeven++
+        }
 
     })
 
@@ -83,34 +121,64 @@ for (layer of layers) {
         let testelement = element["enabled"]
         if (testelement) encount++
         else discount++
-        console.log(testelement)
+        //console.log(testelement)
     })
 
     awruleobj[layer].forEach(element => {
         let testelement = element["hits"]["value"]
-        console.log(testelement)
+       // console.log(testelement)
     })
 
     awruleobj[layer].forEach(element => {
-        console.log(element["rule-number"])
+     //   console.log(element["rule-number"])
         try {
             testdate = element["hits"]["last-date"]["posix"]
             //console.log(element["rule-number"])
             //console.log(element["hits"]["last-date"]["iso-8601"])
-            if (testdate < lastYear) console.log("last year")
-            else if (testdate < lastSixmonth) console.log("last hit in 6-12 month")
-            else if (testdate < lastMonth) console.log("last hit last 1-6 months")
-            else if (testdate < lastSeven) console.log("last hit last 7 days ")
-        } catch(error) {
-            console.log("zero hits")
+            if (testdate < lastYear) {
+                //console.log("last year");
+                hitsLastYear++
+            } else if (testdate < lastSixmonth) {
+                //console.log("last hit in 6-12 month");
+                hitsLastSixmonth++
+            } else if (testdate < lastMonth) {
+                //console.log("last hit last 1-6 months");
+                hitsLastMonth++
+            } else if (testdate < lastSeven) {
+                //console.log("last hit last 7 days ");
+                hitsLastSeven++
+            }
+        } catch (error) {
+            zerocount++;
+            //console.log("zero hits")
         }
     })
 
+    console.log("**********")
+    console.log("Layer Name: ", layer)
+    console.log("Enabled Rules: ", encount)
+    console.log("Disabled Rules: ", discount)
+    console.log("Zero Hit Rules: ", zerocount)
+    console.log("**********")
+    console.log("Rules used in last 7 days", hitsLastSeven)
+    console.log("Rules used in last 30 days", hitsLastMonth)
+    console.log("Rules used in last Last Six Months", hitsLastSixmonth)
+    console.log("Rules used in last Year", hitsLastYear)
+    console.log("**********")
+    console.log("Rules created in last 7 days", createdLastSeven)
+    console.log("Rules created in last 30 days", createdLastMonth)
+    console.log("Rules created in last Last Six Months", createdLastSixmonth)
+    console.log("Rules created in last Year", createdLastYear)
+    console.log("**********")
+    console.log("Rules modified in last 7 days", modifiedLastSeven)
+    console.log("Rules modified in last 30 days", modifiedLastMonth)
+    console.log("Rules modified in last Last Six Months", modifiedLastSixmonth)
+    console.log("Rules modified in last Year", modifiedLastYear)
+
+
 }
 
-console.log("**********")
-console.log(encount)
-console.log(discount)
+
 
 //for (let y in awruleobj[layer]) {
 //    console.log(awruleobj[layer][y]["meta-info"]["last-modify-time"]["posix"])
